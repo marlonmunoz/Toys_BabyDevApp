c94cbfb8351d62c5b482f3d00bb8c01872b7bfa0
Marlon Munoz <marlon@marlonshade.com>
2024-12-08 01:32:06 -0500

    App.jsx fetching

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,3 +1,13 @@
+const { useState, useEffect } = require("react");
+
 function App () {
-    
+   const [toys, setToys] = useState([]);
+   const [searchText, setSearchText] = useState("");
+   const [selectedAge, setSelectedAge] = useState("all");
+   
+   useEffect(() => {
+    fetch("http://localhost:5001/toys")
+        .then(response => response.json())
+        .then(data => setToys(data));
+   }, []);a
 }
